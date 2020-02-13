01bfe14a1c016a104f53e41e46f44ea5b4348514
Jaime Miguel III <jj.miguel21@gmail.com>
2020-02-13 15:31:13 +0800

    added initial class

---
+++ JFMFramework/JFMFramework/Classes/JFMEntityManager.m
@@ -0,0 +1,13 @@
+//
+//  JFMEntityManager.m
+//  JFMFramework
+//
+//  Created by JaimeMiguel on 2/13/20.
+//  Copyright © 2020 M4Guel. All rights reserved.
+//
+
+#import "JFMEntityManager.h"
+
+@implementation JFMEntityManager
+
+@end
