2bf4621ffbb8637aa95730eca8f785e1af60ff43
Jaime Miguel III <jj.miguel21@gmail.com>
2020-02-13 21:14:58 +0800

    added methods

--- JFMFramework/Classes/JFMEntityManager/JFMEntityManager.h
+++ JFMFramework/Classes/JFMEntityManager/JFMEntityManager.h
@@ -11,6 +11,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface JFMEntityManager : NSObject
 
+-(instancetype)initWithDictionaries:(NSDictionary *)dict withSortKeys:(NSArray *)sortKey;
+-(instancetype)initWithArray:(NSArray *)array withSortKeys:(NSArray *)sortKey;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- JFMFramework/Classes/JFMEntityManager/JFMEntityManager.m
+++ JFMFramework/Classes/JFMEntityManager/JFMEntityManager.m
@@ -9,4 +9,25 @@
 
 @implementation JFMEntityManager
 
+-(instancetype)initWithDictionaries:(NSDictionary *)dict withSortKeys:(NSArray *)sortKey {
+    if (self == [super init]) {
+        
+    }
+    return self;
+}
+
+-(instancetype)initWithArray:(NSArray *)array withSortKeys:(NSArray *)sortKey {
+    if (self == [super init]) {
+        
+    }
+    return self;
+}
+
+- (instancetype)init {
+    if (self == [super init]) {
+        
+    }
+    return self;
+}
+
 @end
